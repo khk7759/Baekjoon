525b8d46ebaea18ac1472fc22e289707b8a6bd7d
khk7759 <khk7759@naver.com>
2021-12-18 23:41:47 +0900

    modify next, nextLine의 차이

--- src/AlgorithmStudy/BaekJoon_1152.java
+++ src/AlgorithmStudy/BaekJoon_1152.java
@@ -8,7 +8,9 @@ public class BaekJoon_1152
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String S = sc.nextLine();
+		String S = sc.nextLine(); 
+		// next()는 문자 혹은 문자열을 공백 기준으로 한 단어 또는 한 문자씩 입력을 받는다.
+		// nextLine()의 경우에는 문자 혹은 엔터를 치기 전가지의 문장 전체를 입력을 받는다.
 		
 		StringTokenizer st = new StringTokenizer(S, " ");
 		
